c533477b079d345e854d6aaa58990a1390fcc375
Emmanuel Ruvalcaba <eruvalca@outlook.com>
2021-01-07 15:07:26 -0500

    added weighindeadline migration and updated database

---
+++ Server/Migrations/20210107200648_WeighInDeadlineModelAdd.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WeighUpBlazor.Server.Migrations
+{
+    public partial class WeighInDeadlineModelAdd : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "WeighInDeadlines",
+                columns: table => new
+                {
+                    WeighInDeadlineId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    DeadlineDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    CompetitionId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_WeighInDeadlines", x => x.WeighInDeadlineId);
+                    table.ForeignKey(
+                        name: "FK_WeighInDeadlines_Competitions_CompetitionId",
+                        column: x => x.CompetitionId,
+                        principalTable: "Competitions",
+                        principalColumn: "CompetitionId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_WeighInDeadlines_CompetitionId",
+                table: "WeighInDeadlines",
+                column: "CompetitionId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "WeighInDeadlines");
+        }
+    }
+}
