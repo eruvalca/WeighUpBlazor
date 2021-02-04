b1d588f85e7d7f4ac2048840813b0a43243a1517
Emmanuel Ruvalcaba <eruvalca@outlook.com>
2021-02-04 16:54:27 -0500

    disabled hashing

--- Client/wwwroot/service-worker.published.js
+++ Client/wwwroot/service-worker.published.js
@@ -11,7 +11,7 @@ self.addEventListener('message', event => {
     }
 });
 
-const CACHE_VERSION = 0.13;
+const CACHE_VERSION = 0.14;
 const cacheNamePrefix = 'offline-cache-';
 const cacheName = `${cacheNamePrefix}${self.assetsManifest.version}`;
 const offlineAssetsInclude = [ /\.dll$/, /\.pdb$/, /\.wasm/, /\.html/, /\.js$/, /\.json$/, /\.css$/, /\.woff$/, /\.png$/, /\.jpe?g$/, /\.gif$/, /\.ico$/, /\.blat$/, /\.dat$/ ];
@@ -24,7 +24,9 @@ async function onInstall(event) {
     const assetsRequests = self.assetsManifest.assets
         .filter(asset => offlineAssetsInclude.some(pattern => pattern.test(asset.url)))
         .filter(asset => !offlineAssetsExclude.some(pattern => pattern.test(asset.url)))
-        .map(asset => new Request(asset.url, { integrity: asset.hash }));
+        .map(asset => new Request(asset.url
+            //, { integrity: asset.hash }
+        ));
     await caches.open(cacheName).then(cache => cache.addAll(assetsRequests));
 }
 
