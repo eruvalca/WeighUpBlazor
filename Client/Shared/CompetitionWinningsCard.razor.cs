58de9d720448ca4b13c2895f539140fe3e268fba
eruvalca <eruvalca@outlook.com>
2021-02-10 21:57:43 -0500

    test new update and added winnings card

---
+++ Client/Shared/CompetitionWinningsCard.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WeighUpBlazor.Client.Services;
+using WeighUpBlazor.Shared.Models;
+
+namespace WeighUpBlazor.Client.Shared
+{
+    public partial class CompetitionWinningsCard
+    {
+        [Inject]
+        private CompetitionsService CompetitionsService { get; set; }
+
+        [Parameter]
+        public Competition Competition { get; set; }
+
+        private Dictionary<WeighInDeadline, List<ContestantResultSet>> CompetitionResults = new Dictionary<WeighInDeadline, List<ContestantResultSet>>();
+
+        protected async override Task OnInitializedAsync()
+        {
+            CompetitionResults = Competition.GetResults();
+        }
+    }
+}
